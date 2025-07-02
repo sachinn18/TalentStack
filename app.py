f83d38771e2d3f989557c6a2a34794091515357c
SACHIN JODALLI <sachinjodalli9@gmail.com>
2025-07-02 18:14:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'  # Change this!
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
